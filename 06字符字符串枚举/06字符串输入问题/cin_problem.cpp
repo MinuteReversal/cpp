36f9011274d2ea735291cfdba7d5a3eaed89d1ca
ReversalMinutes <114233763@qq.com>
2020-03-30 07:10:36 +0800

    add code

---
+++ 06字符字符串枚举/06字符串输入问题/cin_problem.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main(int argc,char **argv)
+{  
+	char favor[20];
+	char name[20];
+	cout << "input your name" << endl;
+	cin >> name;
+	cout << "input your favor"<< endl;
+	cin >> favor;
+	cout << "your name " << name << endl;
+	cout << "your favor " << favor << endl;
+   return 0;
+}
