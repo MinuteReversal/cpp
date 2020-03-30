4b0bed435c400cbd8235ca43155fa8680179855e
ReversalMinutes <114233763@qq.com>
2020-03-31 01:38:27 +0800

    add address and pointer demo

---
+++ 01变量/03Point/01address.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int donuts = 6;
+	double cpus = 4.5;
+	cout << "donuts address is " << &donuts << endl;
+	cout << "cpus address is " << &cpus << endl;
+	return 0;
+}
---
+++ 01变量/03Point/02pointer.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int coin = 1;
+	int* pcoin = &coin;
+	cout << "value of coin = " << coin  
+	     << " address of coin = " << &coin << endl;
+	cout << "value of pcoin = " << *pcoin
+	     << " address of pcoin = " << pcoin << endl;
+	return 0;
+}
