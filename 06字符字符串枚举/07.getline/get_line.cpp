3d9f5cb6c4171058d6b03ded151a0bd5ec4759d6
ReversalMinutes <114233763@qq.com>
2020-03-31 00:08:04 +0800

    getline

---
+++ 06字符字符串枚举/07.getline/get_line.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout <<"input your name" <<endl;
+	string name = "";
+	getline(cin,name);
+	cout << "good evening " << name <<endl;
+	return 0;
+}
