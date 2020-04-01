17e1e6a9900b330a3f0360615cbe9fa4a3cf710d
MinuteReversal <114233763@qq.com>
2020-04-01 10:01:32 +0800

    typeid of variable

--- 09class_object/10typid/object_type.cpp
+++ 09class_object/10typid/object_type.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
+#include <typeinfo>
 
 using namespace std;
 
+class Car
+{
+public:
+  string color = "red";
+};
+
 int main(int argc, char **argv)
 {
+  Car *car = new Car();
+  cout << typeid(car).name() << endl;
   return 0;
 }
