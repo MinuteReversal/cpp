f7a7b00e1c8fbc67ef65443a0e7109537dd0e999
MinuteReversal <114233763@qq.com>
2020-12-09 15:24:12 +0800

    control

--- window/control/controls.cpp
+++ window/control/controls.cpp
@@ -1,4 +1,7 @@
-///https://docs.microsoft.com/en-us/windows/win32/learnwin32/managing-application-state-
+//https://docs.microsoft.com/en-us/windows/win32/learnwin32/managing-application-state-
+//http://www.cplusplus.com/forum/windows/90014/
+//http://www.cplusplus.com/forum/windows/11305/
+//https://stackoverflow.com/questions/37271194/wm-command-catch-button-press-in-c-win32
 
 #ifndef UNICODE
 #define UNICODE
@@ -6,19 +9,33 @@
 #include <windows.h>
 #include "../base_window/base_window.h"
 
+#define IDC_BUTTON_1 0x1
+#define IDC_BUTTON_2 0x2
+
 class MainWindow : public BaseWindow<MainWindow>
 {
 public:
   PCWSTR ClassName() const { return L"Sample Window Class"; }
   LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
   void onCreate();
   void onPaint();
+  void Button1_OnClick();
+  HWND button1;
+  HWND button2;
 };
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg)
   {
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDC_BUTTON_1)
+    {
+      Button1_OnClick();
+    }
+    return 0;
   case WM_CREATE:
     onCreate();
     return 0;
@@ -34,10 +51,16 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
   return TRUE;
 }
 
+void MainWindow::Button1_OnClick()
+{
+  MessageBox(m_hwnd, L"clicked", L"title", MB_OK);
+}
+
 void MainWindow::onCreate()
 {
+  //
   //按钮
-  HWND hwndButton = CreateWindow(
+  button1 = CreateWindow(
       L"BUTTON",                                             // Predefined class; Unicode assumed
       L"OK",                                                 // Button text
       WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles
@@ -46,7 +69,20 @@ void MainWindow::onCreate()
       100,                                                   // Button width
       100,                                                   // Button height
       m_hwnd,                                                // Parent window
-      NULL,                                                  // No menu.
+      (HMENU)IDC_BUTTON_1,                                   // For a child window, hMenu specifies the child-window identifier.
+      (HINSTANCE)GetWindowLongPtr(m_hwnd, GWLP_HINSTANCE),
+      NULL);
+
+  button2 = CreateWindow(
+      L"BUTTON",                                             // Predefined class; Unicode assumed
+      L"OK2",                                                // Button text
+      WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles
+      120,                                                   // x position
+      10,                                                    // y position
+      100,                                                   // Button width
+      100,                                                   // Button height
+      m_hwnd,                                                // Parent window
+      (HMENU)IDC_BUTTON_2,                                   // No menu.
       (HINSTANCE)GetWindowLongPtr(m_hwnd, GWLP_HINSTANCE),
       NULL);
 }
@@ -56,12 +92,16 @@ void MainWindow::onCreate()
  */
 void MainWindow::onPaint()
 {
+  PAINTSTRUCT ps;
+  HDC hdc = BeginPaint(m_hwnd, &ps);
+  FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+  EndPaint(m_hwnd, &ps);
 }
 
 /**
  * 入口
  */
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
   MainWindow win;
 
--- window/readme.md
+++ window/readme.md
@@ -1,1 +1,2 @@
 [https://docs.microsoft.com/en-us/windows/win32/api/](https://docs.microsoft.com/en-us/windows/win32/api/)
+[https://github.com/microsoft/Windows-classic-samples/tree/master/Samples](https://github.com/microsoft/Windows-classic-samples/tree/master/Samples)
