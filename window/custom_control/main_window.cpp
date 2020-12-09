0c8ddbd5f6c25281a8bb4274de7782734c618929
MinuteReversal <114233763@qq.com>
2020-12-09 16:33:48 +0800

    custom control

--- window/custom_control/main_window.cpp
+++ window/custom_control/main_window.cpp
@@ -3,18 +3,41 @@
 #endif
 #include <windows.h>
 #include "../base_window/base_window.h"
+#define IDC_STATIC 0x1
 
 class MainWindow : public BaseWindow<MainWindow>
 {
 public:
   PCWSTR ClassName() const { return L"Sample Window Class"; }
   LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+  HWND g_myStatic;
 };
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg)
   {
+  case WM_CREATE:
+    // g_myStatic is a global HWND variable.
+    g_myStatic = CreateWindowEx(0, L"STATIC", L"Some static text",
+                                WS_CHILD | WS_VISIBLE | SS_OWNERDRAW,
+                                25, 125, 150, 20, m_hwnd, 0, 0, 0);
+    return 0;
+  case WM_DRAWITEM:
+  {
+    LPDRAWITEMSTRUCT pDIS = (LPDRAWITEMSTRUCT)lParam;
+    if (pDIS->hwndItem == g_myStatic)
+    {
+      SetTextColor(pDIS->hDC, RGB(100, 0, 100));
+      wchar_t staticText[99];
+      //获取指定hwnd的标题
+      int len = SendMessage(g_myStatic, WM_GETTEXT, (sizeof(staticText) / sizeof(wchar_t)), (LPARAM)staticText);
+      TextOut(pDIS->hDC, pDIS->rcItem.left, pDIS->rcItem.top, staticText, len);
+    }
+  }
+    return TRUE;
   case WM_DESTROY:
     PostQuitMessage(0);
     return 0;
