2493f419ba2b8f9fb97ae44669d58c91a09ed6bc
MinuteReversal <114233763@qq.com>
2020-12-10 18:49:09 +0800

    transparent

--- window/blurring/main_window.cpp
+++ window/blurring/main_window.cpp
@@ -16,13 +16,13 @@ public:
 
 private:
   void Blur(HWND hwnd);
+  void onPaint();
 };
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg)
   {
-  
   case WM_CREATE:
     Blur(m_hwnd);
     return 0;
@@ -33,19 +33,42 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
     DestroyWindow(m_hwnd);
     return 0;
   case WM_PAINT:
-  {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(m_hwnd, &ps);
-    // FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW));
-    EndPaint(m_hwnd, &ps);
-  }
+    onPaint();
     return 0;
   default:
     return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
   }
   return TRUE;
 }
+/**
+ * 画线
+ */
+void MainWindow::onPaint()
+{
+  PAINTSTRUCT ps;
+  HDC hdc = BeginPaint(m_hwnd, &ps);
+  // FillRect(hdc, &ps.rcPaint, (HBRUSH)(BLACK_BRUSH));  
 
+  //画线
+  HPEN hPen = CreatePen(PS_DOT, 1, RGB(255, 0, 0));
+  SelectObject(hdc, hPen);
+  MoveToEx(hdc, 0, 00, (LPPOINT)NULL);
+  LineTo(hdc, 200, 200);
+
+  //弧
+  Arc(hdc, 0, 200, 200, 400, 0, 200, 200, 400);
+
+  //弧
+  ArcTo(hdc, 200, 200, 400, 400, 200, 200, 400, 400);
+
+  //圆
+  Ellipse(hdc, 200, 0, 400, 200);
+
+  //矩形
+  Rectangle(hdc, 400, 0, 600, 200);
+
+  EndPaint(m_hwnd, &ps);
+}
 void MainWindow::Blur(HWND hwnd)
 {
   const auto SetWindowCompositionAttribute =
