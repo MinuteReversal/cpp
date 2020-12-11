ab724b72a1d04927af73daa5bcc8f73114611ddd
MinuteReversal <114233763@qq.com>
2020-12-11 10:36:07 +0800

    bezier

--- window/gdiplus/text.cpp
+++ window/gdiplus/text.cpp
@@ -51,27 +51,11 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 void MainWindow::OnPaint(HDC hdc)
 {
   Gdiplus::Graphics graphics(hdc);
-
-  //设置笔
-  Pen pen(Gdiplus::Color(255, 0, 0, 255));
-  //画线
-  graphics.DrawLine(&pen, 0, 0, 200, 100);
-
-  //画方块
-  graphics.DrawRectangle(&pen, 200, 0, 100, 100);
-
-  //画圆
-  graphics.DrawEllipse(&pen, 300, 0, 100, 100);
-
-  //弧线
-  graphics.DrawArc(&pen, 400, 0, 100, 100, 0, 90);
-
-  //曲线
-  Gdiplus::Point points[] = {Gdiplus::Point(500, 0), Gdiplus::Point(600, 100), Gdiplus::Point(700, 0)};
-  graphics.DrawCurve(&pen, points, ARRAYSIZE(points));
-
-  //贝塞尔曲线
-  graphics.DrawBezier(&pen, 700, 0, 800, 0, 700, 100, 800, 100);
+  SolidBrush brush(Gdiplus::Color(255, 0, 0, 255));
+  FontFamily fontFamily(L"Times New Roman");
+  Font font(&fontFamily, 24, ::FontStyleRegular, ::UnitPixel);
+  PointF pointF(10.0f, 20.0f);
+  graphics.DrawString(L"Hello World!", -1, &font, pointF, &brush);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
@@ -80,7 +64,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 
   if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
   {
-    return 0;
+    return EXIT_SUCCESS;
   }
 
   ShowWindow(win.Window(), nCmdShow);
@@ -94,5 +78,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     DispatchMessage(&msg);
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
