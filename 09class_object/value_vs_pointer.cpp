1e0fe40f039ca16c72ace288a7adeaf204d3df8b
MinuteReversal <114233763@qq.com>
2021-09-23 08:37:43 +0800

    format code

--- 09class_object/value_vs_pointer.cpp
+++ 09class_object/value_vs_pointer.cpp
@@ -2,30 +2,33 @@
 #include <string>
 using namespace std;
 
-class Car {
-  public:
-   string color="red";
-   Car(string color){
-     this->color=color;
-   }
-   void drive()
-   {
-     cout<<this->color<<" car drove away."<<endl;
-   }
+class Car
+{
+public:
+  string color = "red";
+  Car(string color)
+  {
+    this->color = color;
+  }
+  void drive()
+  {
+    cout << this->color << " car drove away." << endl;
+  }
 };
 
-int main(){
-  auto car1=Car("red");
+int main()
+{
+  auto car1 = Car("red");
   car1.drive();
-  auto car2=car1;
-  car2.color="yellow";
+  auto car2 = car1;
+  car2.color = "yellow";
   car1.drive();
   car2.drive();
-  
-  auto cara=new Car("blue");
+
+  auto cara = new Car("blue");
   cara->drive();
-  auto carb=cara;
-  carb->color="orange";
+  auto carb = cara;
+  carb->color = "orange";
   cara->drive();
   carb->drive();
   delete cara;
