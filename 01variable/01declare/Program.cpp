590fb3c41d5497bb03fef554082b74799e41e542
MinuteReversal <114233763@qq.com>
2022-09-15 11:03:39 +0800

    c++ link to lib

--- 01variable/01declare/Program.cpp
+++ 01variable/01declare/Program.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[], char *envp[])
-{
-    char a = 'c';
-    bool b = false;
-    float f = 3.14;
-    return 0;
+int main(int argc, char *argv[], char *envp[]) {
+  char a = 'c';
+  string str = "hello world";
+  bool b = false;
+  float f = 3.14f;
+  double d = 3.0;
+  int integer = 10;
+  int oct = 0077;
+  int binary = 0b11;
+  int hex = 0xFF;
+  return 0;
 }
