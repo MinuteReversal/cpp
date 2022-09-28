241f43229bceef3607fc261f65f40f9ed83c6fb0
MinuteReversal <114233763@qq.com>
2022-09-28 11:33:03 +0800

    atl_window

--- window/atl_window/my_atl_window.cpp
+++ window/atl_window/my_atl_window.cpp
@@ -1,30 +1,11 @@
-#include <atlbase.h>
-#include <atlwin.h>
+
+#include "my_atl_window.h"
 #include <windows.h>
 
 #define IDC_MYATLWINDOWTEST 0x0001
 
-class CMainWindow : public CWindowImpl<CMainWindow> {
-public:
-  BEGIN_MSG_MAP(CMainWindow)
-  MESSAGE_HANDLER(WM_PAINT, OnPaint)
-  MESSAGE_HANDLER(WM_DESTROY, OnDestory)
-  END_MSG_MAP()
-
-  LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled) {
-    PAINTSTRUCT ps;
-    this->BeginPaint(&ps);
-    this->EndPaint(&ps);
-    return 0;
-  }
-
-  LRESULT OnDestory(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled) {
-    PostQuitMessage(0);
-    return 0;
-  }
-};
-
 HINSTANCE hInst;
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstace,
                      LPTSTR lpCmdLine, int nCmdShow) {
   MSG msg;
@@ -33,8 +14,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstace,
   HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDC_MYATLWINDOWTEST));
 
   CMainWindow wnd;
-  wnd.Create(NULL, CWindow::rcDefault, _T("Learn to Program Windows"),
-             WS_OVERLAPPEDWINDOW, WS_EX_CLIENTEDGE, hMenu);
+  wnd.Create(NULL, CWindow::rcDefault, _T("My window"), WS_OVERLAPPEDWINDOW,
+             WS_EX_CLIENTEDGE, hMenu);
   wnd.ShowWindow(nCmdShow);
   wnd.UpdateWindow();
 
