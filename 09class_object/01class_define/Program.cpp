091e7ea9e4a638a474c34bb961c92adb930a5bd4
MinutesReversal <114233763@qq.com>
2023-03-10 09:08:31 +0800

    lowercamelcase class member variable

--- 09class_object/01class_define/Program.cpp
+++ 09class_object/01class_define/Program.cpp
@@ -2,53 +2,36 @@
 
 using namespace std;
 
-class Car
-{
-  private:
-    char *Color;
+class Car {
+private:
+  char *color;
 
-  protected:
-    char *Brand;
+protected:
+  char *brand;
 
-  public:
-    Car()
-    {
-    }
-    Car(char *brand)
-    {
-        this->Brand = brand;
-    }
-    void SetColor(char *Color)
-    {
-        this->Color = Color;
-    }
-    char *GetColor()
-    {
-        return this->Color;
-    }
-    void Run()
-    {
-        cout << "a " << this->Brand << " brand " << this->Color << " color "
-             << " Car are going." << endl;
-    }
-    ~Car()
-    {
-        cout << "The car" << this->Brand << " are releaseed" << endl;
-    }
+public:
+  Car() {}
+  Car(char *brand) { this->brand = brand; }
+  void SetColor(char *Color) { this->color = Color; }
+  char *GetColor() { return this->color; }
+  void Run() {
+    cout << "a " << this->brand << " brand " << this->color << " color "
+         << " Car are going." << endl;
+  }
+  ~Car() { cout << "The car" << this->brand << " are releaseed" << endl; }
 };
 
-int main(int argc, char **argv)
-{
-    //reference type
-    Car *car = new Car("BMW");
-    car->SetColor("red");
-    car->Run();
-    delete car; //hand release
+int main(int argc, char **argv) {
+  // reference type
+  Car *car = new Car((char *)"BMW");
+  car->SetColor(const_cast<char *>("red"));
+  car->Run();
+  delete car; // hand release
 
-    //value type
-    Car car2("WL");
-    car2.SetColor("White");
-    car2.Run();
+  // value type
+  Car car2((char *)"WL");
+  car2.SetColor((char *)"White");
+  car2.Run();
 
-    return 0;
+  return 0;
 }
